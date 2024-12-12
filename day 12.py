34f186a762d667ece9bc2f630923d1dbcb12c0d6
Clover Webster <clover.webster@gmail.com>
2024-12-12 23:05:49 +0000

    part 2

--- day 12.py
+++ day 12.py
@@ -3,11 +3,6 @@ class Regions:
         Region.xpos = xpos
         Region.ypos = ypos
 
-'''
-for u in grid:
-    print(u)
-'''
-
 with open('12.txt') as j:
     data = [i for i in j.read().strip().split("\n")]
 
@@ -24,6 +19,8 @@ for x in range(0,len(grid[0])):
     curr.append('.')
 grid = [curr] + [curr] + grid + [curr] + [curr]
 
+upEdge,downEdge = [],[]
+leftEdge, rightEdge =[],[]
 
 def contains(reg,array):
     for x in range(0,len(array)):
@@ -41,6 +38,14 @@ def move(region):
             newregion = Regions(region.xpos, region.ypos-1)
             if contains(newregion,regions) is False and contains(newregion,newRegions) is False:
                 newRegions.append(newregion)
+        else:
+            if region.ypos-1 in upEdge:
+                allX = upEdge.index(region.ypos-1)+1
+                if region.xpos not in upEdge[allX]:
+                    upEdge[allX].append(region.xpos)
+            else:
+                upEdge.append(region.ypos-1)
+                upEdge.append([region.xpos])
 
     #down
     if region.ypos < len(grid)-1:   
@@ -48,6 +53,14 @@ def move(region):
             newregion = Regions(region.xpos, region.ypos+1)
             if contains(newregion,regions) is False and contains(newregion,newRegions) is False:
                 newRegions.append(newregion)
+        else:
+            if region.ypos+1 in downEdge:
+                allX = downEdge.index(region.ypos+1)+1
+                if region.xpos not in downEdge[allX]:
+                    downEdge[allX].append(region.xpos)
+            else:
+                downEdge.append(region.ypos +1)
+                downEdge.append([region.xpos])
 
     #left
     if region.xpos > 0:   
@@ -55,6 +68,14 @@ def move(region):
             newregion = Regions(region.xpos-1, region.ypos)
             if contains(newregion,regions) is False and contains(newregion,newRegions) is False:
                 newRegions.append(newregion)
+        else:
+            if region.xpos-1 in leftEdge:
+                allY = leftEdge.index(region.xpos-1)+1
+                if region.ypos not in leftEdge[allY]:
+                    leftEdge[allY].append(region.ypos)
+            else:
+                leftEdge.append(region.xpos-1)
+                leftEdge.append([region.ypos])
 
     #right
     if region.xpos < len(grid[0])-1:   
@@ -62,6 +83,14 @@ def move(region):
             newregion = Regions(region.xpos+1, region.ypos)
             if contains(newregion,regions) is False and contains(newregion,newRegions) is False:
                 newRegions.append(newregion)
+        else:
+            if region.xpos+1 in rightEdge:
+                allY = rightEdge.index(region.xpos+1)+1
+                if region.ypos not in rightEdge[allY]:
+                    rightEdge[allY].append(region.ypos)
+            else:
+                rightEdge.append(region.xpos+1)
+                rightEdge.append([region.ypos])
 
 def surrounding(pos):
     count = 0
@@ -76,45 +105,75 @@ def surrounding(pos):
         if contains(Regions(pos.xpos,pos.ypos+1),regions) is True:
             count +=1
         #W
-        if contains(Regions(pos.xpos+1,pos.ypos),regions) is True:
+        if contains(Regions(pos.xpos-1,pos.ypos),regions) is True:
             count +=1
-            '''
-        #NW
-        if contains(Regions(pos.xpos-1,pos.ypos-1),regions) is True:
-            return True
-        #NE
-        if contains(Regions(pos.xpos+1,pos.ypos-1),regions) is True:
-            return True
-        #SE
-        if contains(Regions(pos.xpos+1,pos.ypos+1),regions) is True:
-            return True
-        #SW
-        if contains(Regions(pos.xpos-1,pos.ypos+1),regions) is True:
-            return True
-        '''
     return count
 
 
+def part1():
+    global regions, newRegions
+    visited = []
+    price = 0
+    for line in range(0,len(grid)):
+        for column in range(0,len(grid[0])):
+            cell = grid[line][column]
+            if cell != '.' and contains(Regions(column,line),visited) is False:
+                regions,newRegions = [Regions(column,line)], [1]
+                while len(newRegions) > 0:
+                    newRegions = []
+                    for instance in regions:
+                        move(instance)
+                    if len(newRegions)>0:
+                        visited += newRegions
+                        regions += newRegions
+                area = len(regions)
+                perim = 0
+                for y in range(1,len(grid)-1):
+                    for x in range(1,len(grid[0])-1):
+                        perim += surrounding(Regions(x,y))
+                price += area *perim
+    print('part 1:',price)
+part1()
 
-visited = []
-price = 0
-for line in range(0,len(grid)):
-    print(line,'/', len(grid))
-    for column in range(0,len(grid[0])):
-        cell = grid[line][column]
-        if cell != '.' and contains(Regions(column,line),visited) is False:
-            regions,newRegions = [Regions(column,line)], [1]
-            while len(newRegions) > 0:
-                newRegions = []
-                for instance in regions:
-                    move(instance)
-                if len(newRegions)>0:
-                    visited += newRegions
-                    regions += newRegions
-            area = len(regions)
-            perim = 0
-            for y in range(1,len(grid)-1):
-                for x in range(1,len(grid[0])-1):
-                    perim += surrounding(Regions(x,y))
-            price += area *perim
-print(price)
+
+def peri(array):
+    count = 0
+    for x in range(1,len(array),2):
+        coords = sorted(array[x])
+        prev = -2
+        for y in range(0,len(coords)):
+            if coords[y] != prev + 1:
+                count += 1
+            prev = coords[y]
+    return count
+
+
+def part2():
+    global regions, newRegions
+    global upEdge, downEdge, leftEdge, rightEdge
+    upEdge,downEdge = [],[]
+    leftEdge, rightEdge =[],[]
+    visited = []
+    price = 0
+    for line in range(0,len(grid)):
+        for column in range(0,len(grid[0])):
+            upEdge,downEdge = [],[]
+            leftEdge, rightEdge =[],[]
+            cell = grid[line][column]
+            if cell != '.' and contains(Regions(column,line),visited) is False:
+                regions,newRegions = [Regions(column,line)], [1]
+                while len(newRegions) > 0:
+                    newRegions = []
+                    for instance in regions:
+                        move(instance)
+                    if len(newRegions)>0:
+                        visited += newRegions
+                        regions += newRegions
+                area = len(regions)
+                perim = peri(upEdge) + peri(downEdge)+ peri(leftEdge)+peri(rightEdge)
+                #print('area',area,'per',perim)
+                price += area *perim
+    print('part 2:',price)
+part2()
+
+
